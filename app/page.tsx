f7288e250924745c7a60ac3a65fdbd1df7713b22
Ta-90r <takotansama@outlook.jp>
2026-04-15 17:08:13 +0900

    remove embed

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,7 @@
-"use client";
-
 export default function Home() {
   return (
-    <div style={{ padding: 40 }}>
-      <h1>Fashion LookMatch</h1>
-      <p>トップページOK</p>
+    <div>
+      <h1>OK表示</h1>
     </div>
   );
 }
